22a84aa940084c401c7fca75e02f2f9bebabc6d7
chenshigang <chenshigang@fangcunyisheng.com>
2017-03-10 14:23:58 +0800

    'simple'

--- integrate/src/pages/statistic/patient/PatientTotal.vue
+++ integrate/src/pages/statistic/patient/PatientTotal.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="page-content">
     <div class="row">
-    <div class="col-sm-5 col-sm-offset-1">
+    <div class="col-sm-6">
         <div class="panel panel-default">
               <div class="panel-heading" style="padding:10px 10px">
                   按疾病分布
@@ -10,7 +10,7 @@
               </div>
         </div>
     </div>
-    <div class="col-sm-5">
+    <div class="col-sm-6">
         <div class="panel panel-default">
               <div class="panel-heading" style="padding:10px 10px">
                   按性别分布
@@ -21,7 +21,7 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-sm-10 col-sm-offset-1">
+    <div class="col-sm-12">
         <div class="panel panel-default">
               <div class="panel-heading" style="padding:10px 10px">
                   按年龄分布
