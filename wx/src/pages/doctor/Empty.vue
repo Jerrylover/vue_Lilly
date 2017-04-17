311eeabe13853a135baffd80662785022af256f6
taoxiaojin <qiaotaojin@163.com>
2017-04-17 19:34:26 +0800

    add

---
+++ wx/src/pages/doctor/Empty.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="empty.vue">
+        
+    </div>
+</template>
+<script>
+    export default {
+        data: function() {
+            return {
+
+            }
+        },
+    }
+    //兼容ios8以及以下 #路由之后被消除的落地页进行跳转
+</script>
---
+++ wx/src/pages/doctor/error.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <span>{{errmsg}}</span>
+    </div>
+</template>
+<script>
+    export default {
+        data: function() {
+            return {
+                errmsg: '页面消失不见了，请联系技术人员。',
+            }
+        },
+        created: function() {
+            // this.errmsg = sessionStorage.getItem('_href_');
+        },
+        mounted: function() {
+            console.log('22222');
+            
+        },
+        watch: {
+            '$route': function(to, from) {
+                console.log('11111');
+                console.log(to);
+                this.errmsg = sessionStorage.getItem('_href_');
+            }
+        },
+        beforeRouteEnter(to, from, next) {
+            next();
+        }
+    }
+</script>
